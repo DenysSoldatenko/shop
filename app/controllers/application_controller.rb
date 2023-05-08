dc11ac8b62b4baaff06ea2c9af859e618e663f33
denys <soldatenko911@gmail.com>
2023-05-09 00:38:30 +0300

    Authentication commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(_resource)
+    account_path_url
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
