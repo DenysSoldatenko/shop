7600347bffcb16c5f1a2d9f96f2ef40aa35842f1
VladMahdenko <mahdenko2020ks13@student.karazin.ua>
2023-05-11 20:27:52 +0300

    Added Category controller, disabled required authentication, minor fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource)
     account_path_url
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   belongs_to :parent, class_name: "Category", optional: true
   has_many :children, class_name: "Category", foreign_key: "parent_id"
   has_and_belongs_to_many :products
+
+  def descendants
+    self.children | self.children.map(&:descendants).flatten
+  end
+
 end
