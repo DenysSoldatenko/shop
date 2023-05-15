c8569eaa2775f400a8cdceff1b5a8f0dbf6ffe60
denys <soldatenko911@gmail.com>
2023-05-15 22:38:16 +0300

    Added new data to database

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@
 # This is the Product class
 class Product < ApplicationRecord
   has_and_belongs_to_many :categories
-  has_many :attributes, through: :attributes_products
+  has_many :attributes_products
+  has_many :product_attributes, through: :attributes_products, class_name: 'Attribute'
 end
